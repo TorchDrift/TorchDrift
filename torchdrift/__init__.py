dea23e3d2b307e86fd50b7e02cc20d07f17123e9
Thomas Viehmann <tv.code@beamnet.de>
2021-02-17 14:17:20 +0100

    add submodule imports

--- torchdrift/__init__.py
+++ torchdrift/__init__.py
@@ -1,3 +1,3 @@
-from . import data, detectors, utils
+from . import data, detectors, utils, reducers
 
 # Todo. :)
--- torchdrift/detectors/__init__.py
+++ torchdrift/detectors/__init__.py
@@ -1,2 +1,3 @@
 from .detector import DriftDetector
 from .mmd import kernel_mmd, KernelMMDDriftDetector
+from .ks import ks_two_sample_multi_dim, KSDriftDetector
