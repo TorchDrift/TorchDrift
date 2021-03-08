1bdf7cdc96e4a53c9077577c959522fedf365f69
Thomas Viehmann <tv.code@beamnet.de>
2021-03-08 13:52:00 +0100

    pypi data

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 import setuptools
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
   name='torchdrift',
-  version='0.1.0',
+  version='0.1.0.post1',
   description="Drift Detection for PyTorch",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author='Orobix Srl and MathInf GmbH',
+  url='https://torchdrift.org/',
   install_requires=['torch'],
-  packages=setuptools.find_packages()
+  packages=setuptools.find_packages(),
   )
