c26d8a5fb85aef6f5a34eba4e86264b9b1608f3d
Thomas Viehmann <tv.code@beamnet.de>
2021-03-24 14:30:13 +0100

    Add favicon / logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,15 +42,12 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = 'favicon.svg'
+html_logo = '_static/logo/torchdrift-rendered.svg'
 
 mathjax_path = "mathjax/tex-chtml.js"
 
